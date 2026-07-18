604c3bb64049ed07b8088e38d53457adbf13332c
Siddhartha Gautam <sid.2005.19@gmail.com>
2026-07-18 16:52:16 +0530

    Setup the server and mongoDB successfully.

---
+++ server/src/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const app = require("./app");
+const connectDB = require("./config/db");
+
+const PORT = process.env.PORT || 4001;
+
+const startServer = async () => {
+    try{
+        await connectDB();
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        })
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+    }
+}
+
+startServer();
